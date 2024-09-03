41e2b0dea78ce3e5d42f97b3bb9a81bcddf5a7e9
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2024-09-03 10:59:17 +0200

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python3
+""" 3. Auth class
 """
-Manage API authentication system
-"""
+
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
+    """Authentication class
     """
-    Manage API authentication methods
-    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Return boolean """
-        if path is None or excluded_paths is None or not len(excluded_paths):
+        """ def require_auth
+        """
+        if not path or not excluded_paths:
             return True
-
-        if path[-1] != '/':
-            path += '/'
-        if excluded_paths[-1] != '/':
-            excluded_paths += '/'
-
-        astericks = [stars[:-1]
-                     for stars in excluded_paths if stars[-1] == '*']
-
-        for stars in astericks:
-            if path.startswith(stars):
+        path = path + '/' if path[-1] != '/' else path
+        has_wildcard = any(x.endswith("*") for x in excluded_paths)
+        if not has_wildcard:
+            return path not in excluded_paths
+        for e in excluded_paths:
+            if e.endswith("*"):
+                if path.startswith(e[:-1]):
+                    return False
+            if path == e:
                 return False
-
-        if path in excluded_paths:
-            return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Request Flask object """
-        if request is None or 'Authorization' not in request.headers:
-            return None
-        return request.headers.get('Authorization')
+        """ def authorization_header
+        """
+        if request:
+            return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Flask request object """
+        """ def curent_user
+        """
         return None
-
